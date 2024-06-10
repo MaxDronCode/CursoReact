7a0371f82599e52c115849fc7d5097a241112d8f
Max <Max>
2024-06-10 20:41:43 +0200

    Algunos avances

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
 export function App(){
+
     return (
-        <article className="tw-followCard">
-            <header className="tw-followCard-header">
-                <img className="tw-followCard-avatar" src="https://unavatar.io/midudev" alt="El avatar de midudev" />
-                <div className="tw-followCard-info">
-                    <strong className="tw-followCard-infoUserName">Miguel Angel Duran</strong>
-                    <span >@midudev</span>
-                </div>
-            </header>
-            <aside>
-                <button className="tw-followCard-button">
-                    Seguir
-                </button>
-            </aside>
-        </article>
+        <section className='App'>
+            <TwitterFollowCard  userName ={'midudev'} name={'Miguel Angel Duran'} />
+            <TwitterFollowCard  userName ={'picuu'} name={'Carlos Capo'} />
+            <TwitterFollowCard  userName={'maxdroncode'} name={'Max Dron'}  />
+            <TwitterFollowCard  userName={'ulisescastell'} name={'Ulisses Castell'}  />
+            <TwitterFollowCard  userName={'david_garcia'} name={'David Garcia'}  />
+        </section>
     )
 }
--- src/TwitterFollowCard.jsx
+++ src/TwitterFollowCard.jsx
@@ -1,17 +1,21 @@
 import './App.css'
 export function TwitterFollowCard({userName, name, isFollowing}){
+    const text = isFollowing ? 'Siguiendo' : 'Seguir'
+    const buttonClassName = isFollowing
+    ? 'tw-followCard-button is-following'
+    : 'tw-followCard-button'
     return (
         <article className="tw-followCard">
         <header className="tw-followCard-header">
             <img className="tw-followCard-avatar" src={`https://unavatar.io/${userName}`} alt="El avatar de midudev" />
             <div className="tw-followCard-info">
-                <strong className="tw-followCard-infoUserName">{name    }</strong>
-                <span >@{userName}</span>
+                <strong>{name}</strong>
+                <span className='tw-followCard-infoUserName'>@{userName}</span>
             </div>
         </header>
         <aside>
-            <button className="tw-followCard-button">
-                Seguir
+            <button className={buttonClassName}>
+                {text}
             </button>
         </aside>
     </article>
